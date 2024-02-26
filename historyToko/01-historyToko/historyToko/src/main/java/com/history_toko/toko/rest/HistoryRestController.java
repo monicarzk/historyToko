cf60c955447ab3731bfdee0cd4369b63c60f5348
monicarzk <monikarizki1933@gmail.com>
2024-02-26 21:39:20 +0700

    buat mapping untuk db histories

--- historyToko/01-historyToko/historyToko/src/main/java/com/history_toko/toko/rest/HistoryRestController.java
+++ historyToko/01-historyToko/historyToko/src/main/java/com/history_toko/toko/rest/HistoryRestController.java
@@ -3,9 +3,7 @@ package com.history_toko.toko.rest;
 import com.history_toko.toko.dao.HistoryDAO;
 import com.history_toko.toko.entity.History;
 import com.history_toko.toko.service.HistoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +22,48 @@ public class HistoryRestController {
         return historyService.findAll();
     }
 
-    
+    @GetMapping("/histories/{historyId}")
+    public History getHistory(@PathVariable int historyId) {
+
+        History theHistory = historyService.findByID(historyId);
+
+        if (theHistory == null) {
+            throw new RuntimeException("History id not found - " + historyId);
+        }
+
+        return theHistory;
+    }
+
+    @PostMapping("/histories")
+    public History addHistory (@RequestBody History theHistory) {
+
+        theHistory.setId(0);
+
+        History dbHistory = historyService.save(theHistory);
+
+        return dbHistory;
+    }
+
+    @PutMapping("/histories")
+    public History updateHistory(@RequestBody History theHistory) {
+
+        History dbHistory = historyService.save(theHistory);
+
+        return dbHistory;
+    }
+
+    @DeleteMapping("/histories/{historyId}")
+    public String deleteHistory(@PathVariable int historyId) {
+
+        History tempHistory = historyService.findByID(historyId);
+
+        if (tempHistory == null) {
+            throw new RuntimeException("History id not found - " + historyId);
+        }
+
+        historyService.deleteById(historyId);
+
+        return "Deleted history id - " + historyId;
+    }
+
 }
