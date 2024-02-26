45c21f4e0609c16e5bab3a55d62fc6ac093241e1
monicarzk <monikarizki1933@gmail.com>
2024-02-26 14:56:26 +0700

    membuat project dengan nama historyToko

---
+++ historyToko/01-historyToko/historyToko/src/main/java/com/history_toko/caringin/CaringinApplication.java
@@ -0,0 +1,13 @@
+package com.history_toko.caringin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaringinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaringinApplication.class, args);
+	}
+
+}
