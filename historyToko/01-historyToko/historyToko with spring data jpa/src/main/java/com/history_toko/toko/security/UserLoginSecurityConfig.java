41cfc638eebd50de99c7cc109a826115349ce775
monicarzk <monikarizki1933@gmail.com>
2024-02-27 11:55:22 +0700

    API Security untuk user_login

--- historyToko/01-historyToko/historyToko with spring data jpa/src/main/java/com/history_toko/toko/security/UserLoginSecurityConfig.java
+++ historyToko/01-historyToko/historyToko with spring data jpa/src/main/java/com/history_toko/toko/security/UserLoginSecurityConfig.java
@@ -1,4 +1,36 @@
 package com.history_toko.toko.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
 public class UserLoginSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}root")
+                .roles("admin")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}root")
+                .roles("user")
+                .build();
+
+        UserDetails owner = User.builder()
+                .username("owner")
+                .password("{noop}root")
+                .roles("owner")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user, owner);
+
+    }
 }
